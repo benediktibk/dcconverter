63d3f92b91a07103317bd709715ffcb22352c31b
Benedikt Schmidt <benediktibk@outlook.com>
2019-12-03 09:27:31 +0100

    Fix Tests for ResistorInSeriesAndInductance

--- DcConverterControllerOptimization/DcConverterControllerOptimization/CircuitSimulationTest/ResistorInSeriesAndInductanceTest.cs
+++ DcConverterControllerOptimization/DcConverterControllerOptimization/CircuitSimulationTest/ResistorInSeriesAndInductanceTest.cs
@@ -8,12 +8,21 @@ namespace CircuitSimulationTest
     public class ResistorInSeriesAndInductanceTest
     {
         [TestMethod]
-        public void CalculateOutputVoltage_0InitialVoltage05s_CorrectVoltage() {
+        public void CalculateOutputVoltage_0InitialVoltage253ms_CorrectVoltage() {
             var circuit = new ResistorInSeriesAndInductance(2, 3, 0.9, 0, 10);
 
-            var outputVoltage = circuit.CalculateOutputVoltage(0.5);
+            var outputVoltage = circuit.CalculateOutputVoltage(0.253018705);
 
-            outputVoltage.Should().BeApproximately(3.728097, 1e-5);
+            outputVoltage.Should().BeApproximately(3.018847, 1e-3);
+        }
+
+        [TestMethod]
+        public void CalculateOutputVoltage_2InitialVoltage143ms_CorrectVoltage() {
+            var circuit = new ResistorInSeriesAndInductance(2, 3, 0.9, 2, 10);
+
+            var outputVoltage = circuit.CalculateOutputVoltage(0.14350996);
+
+            outputVoltage.Should().BeApproximately(3.098410, 1e-3);
         }
     }
 }
