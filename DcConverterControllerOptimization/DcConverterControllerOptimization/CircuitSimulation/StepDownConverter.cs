5a677f6df604b4cc193158f0eeb0291d0ecd153d
Benedikt Schmidt <benediktibk@outlook.com>
2019-12-03 11:25:57 +0100

    Implement StepDownConverter

--- DcConverterControllerOptimization/DcConverterControllerOptimization/CircuitSimulation/StepDownConverter.cs
+++ DcConverterControllerOptimization/DcConverterControllerOptimization/CircuitSimulation/StepDownConverter.cs
@@ -11,18 +11,20 @@ namespace CircuitSimulation
         private readonly double _capacitor;
         private readonly double _inductance;
         private readonly double _outputVoltageInitial;
+        private readonly double _outputVoltageInitialGradient;
         private readonly double _inputVoltage;
 
         #endregion
 
         #region constructor
 
-        public StepDownConverter(double loadResistor, double seriesResistor, double capacitor, double inductance, double outputVoltageInitial, double inputVoltage) {
+        public StepDownConverter(double loadResistor, double seriesResistor, double capacitor, double inductance, double outputVoltageInitial, double outputVoltageInitialGradient, double inputVoltage) {
             _loadResistor = loadResistor;
             _seriesResistor = seriesResistor;
             _capacitor = capacitor;
             _inductance = inductance;
             _outputVoltageInitial = outputVoltageInitial;
+            _outputVoltageInitialGradient = outputVoltageInitialGradient;
             _inputVoltage = inputVoltage;
         }
 
@@ -31,10 +33,35 @@ namespace CircuitSimulation
         #region public functions
 
         public double CalculateOutputVoltage(double time) {
-            var lambda = (-1) * (_seriesResistor + _loadResistor) / (_seriesResistor * _loadResistor * _capacitor);
-            var offset = _loadResistor * _inputVoltage / (_seriesResistor + _loadResistor);
-            var k = _outputVoltageInitial - offset;
-            return k * Math.Exp(lambda * time) + offset;
+            var alpha = _inductance * _capacitor;
+            var beta = (_inductance + _seriesResistor * _loadResistor * _capacitor) / _loadResistor;
+            var gamma = (_loadResistor + _seriesResistor) / _loadResistor;
+            var radicand = beta * beta - 4 * alpha * gamma;
+
+            if (radicand > 0)
+                return CalculateOutputVoltageAperiodic(time, alpha, beta, gamma, radicand);
+            else
+                return CalculateOutputVoltagePeriodic(time, alpha, beta, gamma, radicand);
+        }
+
+        #endregion
+
+        #region private functions
+
+        private double CalculateOutputVoltageAperiodic(double time, double alpha, double beta, double gamma, double radicand) {
+            var lambda1 = ((-1) * beta + Math.Sqrt(radicand)) / (2 * alpha);
+            var lambda2 = ((-1) * beta - Math.Sqrt(radicand)) / (2 * alpha);
+            var k2 = (_outputVoltageInitialGradient - lambda1 * _outputVoltageInitial + _inputVoltage * lambda1 / gamma) / (lambda2 - lambda1);
+            var k1 = _outputVoltageInitialGradient / lambda1 - lambda2 / lambda1 * k2;
+            return k1 * Math.Exp(lambda1 * time) + k2 * Math.Exp(lambda2 * time) + _inputVoltage / gamma;
+        }
+
+        private double CalculateOutputVoltagePeriodic(double time, double alpha, double beta, double gamma, double radicand) {
+            var a = (-1) * beta / (2 * alpha);
+            var b = Math.Sqrt((-1) * radicand) / (2 * alpha);
+            var k1 = _outputVoltageInitial - _inputVoltage / gamma;
+            var k2 = (_outputVoltageInitialGradient - a * k1) / b;
+            return k1 * Math.Exp(a * time) * Math.Cos(b * time) + k2 * Math.Exp(a * time) * Math.Sin(b * time) + _inputVoltage / gamma;
         }
 
         #endregion
--- DcConverterControllerOptimization/DcConverterControllerOptimization/CircuitSimulationTest/StepDownConverterTest.cs
+++ DcConverterControllerOptimization/DcConverterControllerOptimization/CircuitSimulationTest/StepDownConverterTest.cs
@@ -8,21 +8,21 @@ namespace CircuitSimulationTest
     public class StepDownConverterTest
     {
         [TestMethod]
-        public void CalculateOutputVoltage_0InitialVoltageAnd894ms_CorrectVoltage() {
-            var circuit = new StepDownConverter(2, 0.2, 0.220, 0.900, 0, 10);
+        public void CalculateOutputVoltage_Periodic0InitialVoltageAnd894ms_CorrectVoltage() {
+            var circuit = new StepDownConverter(2, 0.2, 0.220, 0.900, 0, 0, 10);
 
             var outputVoltage = circuit.CalculateOutputVoltage(0.894518705);
 
-            outputVoltage.Should().BeApproximately(7.91872, 1e-3);
+            outputVoltage.Should().BeApproximately(7.91872, 3e-3);
         }
 
         [TestMethod]
-        public void CalculateOutputVoltage_2InitialVoltageAnd507ms_CorrectVoltage() {
-            var circuit = new StepDownConverter(2, 0.2, 0.220, 0.900, 2, 10);
+        public void CalculateOutputVoltage_Periodic2InitialVoltageAnd507ms_CorrectVoltage() {
+            var circuit = new StepDownConverter(2, 0.2, 0.220, 0.900, 2, 0, 10);
 
             var outputVoltage = circuit.CalculateOutputVoltage(0.507509959);
 
-            outputVoltage.Should().BeApproximately(5.107602, 1e-3);
+            outputVoltage.Should().BeApproximately(5.107602, 2e-3);
         }
     }
 }
