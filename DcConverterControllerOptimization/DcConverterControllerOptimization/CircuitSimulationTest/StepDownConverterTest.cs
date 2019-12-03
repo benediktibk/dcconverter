c73f3127226d5694bd21e0dc0d23088d84520815
Benedikt Schmidt <benediktibk@outlook.com>
2019-12-03 12:16:09 +0100

    Add Tests for sequential Circuit use

--- DcConverterControllerOptimization/DcConverterControllerOptimization/CircuitSimulationTest/StepDownConverterTest.cs
+++ DcConverterControllerOptimization/DcConverterControllerOptimization/CircuitSimulationTest/StepDownConverterTest.cs
@@ -33,5 +33,29 @@ namespace CircuitSimulationTest
 
             outputVoltage.Should().BeApproximately(3.09207, 2e-3);
         }
+
+        [TestMethod]
+        public void CalculateOutputVoltage_AperiodicWithPulse_CorrectVoltage() {
+            var circuitOn = new StepDownConverter(2, 3, 0.220, 0.100, 2, 0, 10);
+            var offInitialVoltage = circuitOn.CalculateOutputVoltage(0.2);
+            var offInitialVoltageGradient = circuitOn.CalculateOutputVoltageGradient(0.2);
+            var circuitOff = new StepDownConverter(2, 3, 0.220, 0.100, offInitialVoltage, offInitialVoltageGradient, 5);
+
+            var outputVoltage = circuitOff.CalculateOutputVoltage(0.489500);
+
+            outputVoltage.Should().BeApproximately(3.481622, 1e-3);
+        }
+
+        [TestMethod]
+        public void CalculateOutputVoltage_PeriodicWithPulse_CorrectVoltage() {
+            var circuitOn = new StepDownConverter(2, 0.2, 0.220, 0.900, 2, 0, 10);
+            var offInitialVoltage = circuitOn.CalculateOutputVoltage(0.2);
+            var offInitialVoltageGradient = circuitOn.CalculateOutputVoltageGradient(0.2);
+            var circuitOff = new StepDownConverter(2, 0.2, 0.220, 0.900, offInitialVoltage, offInitialVoltageGradient, 2.2);
+
+            var outputVoltage = circuitOff.CalculateOutputVoltage(0.994500);
+
+            outputVoltage.Should().BeApproximately(3.426294, 1e-3);
+        }
     }
 }
