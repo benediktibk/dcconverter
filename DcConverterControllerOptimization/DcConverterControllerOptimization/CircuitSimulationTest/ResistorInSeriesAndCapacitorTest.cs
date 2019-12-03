81af87b0d8b68bbf16a6c971a39b9102365bc858
Benedikt Schmidt <benediktibk@outlook.com>
2019-12-03 09:32:47 +0100

    Add Test for ResistorInSeriesAndCapacitor

--- DcConverterControllerOptimization/DcConverterControllerOptimization/CircuitSimulationTest/ResistorInSeriesAndCapacitorTest.cs
+++ DcConverterControllerOptimization/DcConverterControllerOptimization/CircuitSimulationTest/ResistorInSeriesAndCapacitorTest.cs
@@ -8,12 +8,21 @@ namespace CircuitSimulationTest
     public class ResistorInSeriesAndCapacitorTest
     {
         [TestMethod]
-        public void CalculateOutputVoltage_0InitialVoltage05s_CorrectVoltage() {
+        public void CalculateOutputVoltage_0InitialVoltage406ms_CorrectVoltage() {
             var circuit = new ResistorInSeriesAndCapacitor(2, 3, 0.3, 0, 10);
 
-            var outputVoltage = circuit.CalculateOutputVoltage(0.5);
+            var outputVoltage = circuit.CalculateOutputVoltage(0.406299128);
 
-            outputVoltage.Should().BeApproximately(2.99004, 1e-5);
+            outputVoltage.Should().BeApproximately(2.705910, 1e-3);
+        }
+
+        [TestMethod]
+        public void CalculateOutputVoltage_2InitialVoltage347ms_CorrectVoltage() {
+            var circuit = new ResistorInSeriesAndCapacitor(2, 3, 0.3, 2, 10);
+
+            var outputVoltage = circuit.CalculateOutputVoltage(0.347079605);
+
+            outputVoltage.Should().BeApproximately(3.237300, 1e-3);
         }
     }
 }
