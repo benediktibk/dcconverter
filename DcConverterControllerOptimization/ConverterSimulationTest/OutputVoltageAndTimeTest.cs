8c9ab1d55bfd0229182c28641d5790ee719ed737
Benedikt Schmidt <benediktibk@outlook.com>
2019-12-08 13:53:40 +0100

    Add Test for Matching of Data Series

--- DcConverterControllerOptimization/ConverterSimulationTest/OutputVoltageAndTimeTest.cs
+++ DcConverterControllerOptimization/ConverterSimulationTest/OutputVoltageAndTimeTest.cs
@@ -70,5 +70,37 @@ namespace ConverterSimulationTest {
             result[2].Item2.Time.Should().Be(3.75);
             result[2].Item2.OutputVoltage.Should().Be(1);
         }
+
+        [TestMethod]
+        public void MatchClosestTimesOfSecondOne_SecondListLonger_ItemsInCorrectOrder() {
+            var first = new List<OutputVoltageAndTime> {
+                new OutputVoltageAndTime { Time = 0, OutputVoltage = 3 },
+                new OutputVoltageAndTime { Time = 4, OutputVoltage = 2 },
+                new OutputVoltageAndTime { Time = 1, OutputVoltage = 6 }
+            };
+            var second = new List<OutputVoltageAndTime> {
+                new OutputVoltageAndTime { Time = 0.25, OutputVoltage = 5 },
+                new OutputVoltageAndTime { Time = 0.75, OutputVoltage = 1 },
+                new OutputVoltageAndTime { Time = 1.5, OutputVoltage = 9 },
+                new OutputVoltageAndTime { Time = 3.75, OutputVoltage = 54 },
+                new OutputVoltageAndTime { Time = 9, OutputVoltage = -4 }
+            };
+
+            var result = OutputVoltageAndTime.MatchClosestTimesOfSecondOne(first, second);
+
+            result.Count().Should().Be(3);
+            result[0].Item1.Time.Should().Be(0);
+            result[0].Item1.OutputVoltage.Should().Be(3);
+            result[0].Item2.Time.Should().Be(0.25);
+            result[0].Item2.OutputVoltage.Should().Be(5);
+            result[1].Item1.Time.Should().Be(1);
+            result[1].Item1.OutputVoltage.Should().Be(6);
+            result[1].Item2.Time.Should().Be(0.75);
+            result[1].Item2.OutputVoltage.Should().Be(1);
+            result[2].Item1.Time.Should().Be(4);
+            result[2].Item1.OutputVoltage.Should().Be(2);
+            result[2].Item2.Time.Should().Be(3.75);
+            result[2].Item2.OutputVoltage.Should().Be(54);
+        }
     }
 }
