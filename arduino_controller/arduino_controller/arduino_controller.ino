e683b567007b4fb1d271e2b5a892c1cfb4379af2
Benedikt Schmidt <benediktibk@outlook.com>
2020-10-09 20:07:27 +0200

    Use high speed timer

--- arduino_controller/arduino_controller/arduino_controller.ino
+++ arduino_controller/arduino_controller/arduino_controller.ino
@@ -3,6 +3,31 @@ bool state = false;
 void setup() {
   pinMode(13, OUTPUT);
 
+  // as we use a 16MHZ clock the PLL input frequency has to be divided by 2
+  PLLCSR |= (1<<PINDIV);
+  // enable PLL
+  PLLCSR |= (1<<PLLE);
+
+  // wait until the PLL is locked
+  bool locked = false;
+  do {
+    locked = PLLCSR & (1<<PLOCK);
+  } while (!locked);
+
+  // configure the PLL frequency
+  PLLFRQ = 0;
+  PLLFRQ |= (0<<PINMUX);
+  // divide the PLL frequency by 2 for the USB module -> 48 MHz
+  PLLFRQ |= (1<<PLLUSB);
+  // divide the PLL frequency by 1.5 for the high speed timer -> 64 MHz
+  PLLFRQ |= (0<<PLLTM0);
+  PLLFRQ |= (1<<PLLTM1);
+  // set the PLL frequency to 96MHz
+  PLLFRQ |= (0<<PDIV0);
+  PLLFRQ |= (1<<PDIV1);
+  PLLFRQ |= (1<<PDIV2);
+  PLLFRQ |= (1<<PDIV3);
+
   // count up to the value of OCR4C
   TCCR4D = 0;
 
@@ -19,7 +44,7 @@ void setup() {
   // activate timer 4 interrupt when it returns to 0
   TIMSK4 |= (1<<TOIE4);
   
-  // set the prescaler of timer 4 to /1 -> 0,977 ms per count
+  // set the prescaler of timer 4 to /1 -> 59,604 ns per count
   TCCR4B = 0;
   TCCR4B |= (1<<CS40);
   TCCR4B |= (0<<CS41);
