8d274f078a71e6027bb31e5f7bd32284441e80ec
Benedikt Schmidt <benediktibk@outlook.com>
2020-10-11 13:28:30 +0200

    Implement basic P-Controller

--- arduino_controller/arduino_controller/arduino_controller.ino
+++ arduino_controller/arduino_controller/arduino_controller.ino
@@ -1,3 +1,13 @@
+#define DEBUGLOGGING
+
+#ifdef DEBUGLOGGING
+#define LOGVALUE(description, value, unit) Serial.print(description); Serial.print(": "); Serial.print(value); Serial.print(" "); Serial.println(unit);
+#endif
+
+#ifndef DEBUGLOGGING
+#define LOGVALUE((description, value, unit) ;
+#endif
+
 enum CountLength {
   CountLengthFast, // 15ns
   CountLengthSlow // 256us
@@ -5,6 +15,10 @@ enum CountLength {
 
 const CountLength countLength = CountLengthSlow;
 const bool outputInverted = true;
+const float conversionFactorInputValue = 5.0/1024;
+const float targetValue = 2.4;
+const float kp = 0.01;
+const int maximumCount = 671;
 
 void setup() {
   pinMode(12, OUTPUT);
@@ -42,8 +56,8 @@ void setup() {
   OCR4A = 67;
   
   // set 671 as the maximum value of timer 4
-  TC4H = 0x02;
-  OCR4C = 0x9F;
+  TC4H = (maximumCount>>8);
+  OCR4C = (maximumCount&255);
   
   // set the prescaler of timer 4 to adjust the frequency, based on the input frequency of 64 MHz of the PLL
   TCCR4B = 0;
@@ -72,7 +86,30 @@ void setup() {
     TCCR4A |= (0<<COM4A0);
     TCCR4A |= (1<<COM4A1);
   }
+
+  // Configure serial communication
+  Serial.begin(9600);
 }
 
 void loop() { 
+  float currentValue = analogRead(0)*conversionFactorInputValue;
+  LOGVALUE("current value", currentValue, "V");
+  float error = targetValue - currentValue;
+  float onTimeInPercent = kp * error;
+
+  int onTimeConverted = onTimeInPercent * maximumCount;
+
+  if (onTimeConverted < 0) {
+    onTimeConverted = 0;
+  }
+  else if (onTimeConverted >= maximumCount) {
+    onTimeConverted = maximumCount - 1;
+  }
+
+  LOGVALUE("on time count", onTimeConverted, "steps");
+  
+  byte valueToSet = onTimeConverted;
+  //TC4H = (onTimeConverted>>8);
+  //OCR4A = (onTimeConverted&255);
+  delay(100);
 }
